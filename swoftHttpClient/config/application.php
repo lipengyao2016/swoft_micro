4990795d955279c5d3de17b00eef0b8a0e9d51b8
xiaoling@zhu <1163335306@qq.com>
2019-10-18 17:17:16 +0800

    ff

--- swoftHttpClient/config/application.php
+++ swoftHttpClient/config/application.php
@@ -6,6 +6,6 @@
   'redis_host' => '192.168.5.59',
   'db_password' => '123456',
   'swoft_server_host' => 'test-swoft-server'/*'192.168.5.59'*/,
-  'swoft_server_http_port' => 31000 /*8050*/,
-  'swoft_server_rpc_port' => 31001 /*8051*/,
+  'swoft_server_http_port' => 18306 /*8050*/,
+  'swoft_server_rpc_port' => 18307 /*8051*/,
 );
