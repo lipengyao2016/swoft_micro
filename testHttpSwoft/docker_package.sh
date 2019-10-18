a7430aee889cc777bd63cbb3c3060b944ad0d79c
xiaoling@zhu <1163335306@qq.com>
2019-10-18 15:00:22 +0800

    aa

--- testHttpSwoft/docker_package.sh
+++ testHttpSwoft/docker_package.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker stop myswoft && docker rm myswoft
-docker build -t registry.cn-shenzhen.aliyuncs.com/shenj/swoft_server:1.2 .
+docker build -t registry.cn-shenzhen.aliyuncs.com/shenj/swoft_server:1.3 .
